2b3ed85e6f690f689183f9219f3793abf0466d77
MMaithreyan <maithreyanm8@gmail.com>
2022-05-18 18:31:57 +0530

    code added

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const details = document.querySelectorAll("details");
 details.forEach((targetDetail) => {
   targetDetail.addEventListener("click", () => {
     // Close all the details that are not targetDetail.
+    targetDetail.classList.toggle("active");
     details.forEach((detail) => {
       if (detail !== targetDetail) {
         detail.removeAttribute("open");
@@ -13,28 +14,4 @@ details.forEach((targetDetail) => {
   });
 });
 
-//const img=document.getElementById("plus");
-//const summary=document.getElementById("sum");
-//summary.addEventListener("click",function(){
-//img.classList.toggle("fa-xmark");
-//});
 
-const img = document.querySelectorAll("summary");
-const icon = document.querySelectorAll("#plus1", "#plus2", "plus3");
-img.forEach((change) => {
-  change.addEventListener("click", () => {
-    icon.forEach((xmark) => {
-      xmark.classList.toggle("fa-xmark");
-    });
-  });
-});
-
-
-
-//const img = document.querySelectorAll("summary");
-//const icon = document.querySelectorAll("#plus1", "#plus2", "plus3");
-//for (i = 0; i < img.length; i++) {
- // img[i].addEventListener("click", () => {
-   // icon.classList.toggle("fa-xmark");
-  //});
-//}
