76fe79be3b1b71eb8d9a8929eaf22b4b0399eaa6
MMaithreyan <maithreyanm8@gmail.com>
2022-05-19 15:10:32 +0530

    code added

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 // Fetch all the details element.
 const details = document.querySelectorAll("details");
-
 // Add the onclick listeners.
 details.forEach((targetDetail) => {
   targetDetail.addEventListener("click", () => {
     // Close all the details that are not targetDetail.
-    targetDetail.classList.toggle("active");
     details.forEach((detail) => {
       if (detail !== targetDetail) {
         detail.removeAttribute("open");
