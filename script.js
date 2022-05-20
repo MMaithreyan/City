f07f8f6975e8d9a2f0af1a56ec18279377ffcfb4
MMaithreyan <maithreyanm8@gmail.com>
2022-05-20 10:35:58 +0530

    code added

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const details = document.querySelectorAll("details");
 details.forEach((targetDetail) => {
   targetDetail.addEventListener("click", () => {
     // Close all the details that are not targetDetail.
-        details.forEach((detail) => {
+    details.forEach((detail) => {
       if (detail !== targetDetail) {
         detail.removeAttribute("open");
       }
